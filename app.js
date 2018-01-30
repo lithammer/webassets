00eed0bda53f8e8ee85711bfb3396f00fd3b2ba3
Peter Renström <renstrom.peter@gmail.com>
2018-01-30 10:20:04 +0100

    Fix issues found during vulnerability update

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-var express = require('express'),
-    http = require('http'),
-    path = require('path'),
-    less = require('less'),
-    lessMiddleware = require('less-middleware'),
-    stylus = require('stylus'),
-    uglifyJS = require('uglify-js'),
-    cleanCSS = require('clean-css'),
-    coffeeScript = require('coffee-script');
+var express = require("express"),
+  http = require("http"),
+  path = require("path"),
+  less = require("less"),
+  lessMiddleware = require("less-middleware"),
+  stylus = require("stylus"),
+  uglifyJS = require("uglify-js"),
+  cleanCSS = require("clean-css"),
+  coffeeScript = require("coffee-script");
 
 var app = express();
 module.exports = app;
 
 app.configure(function() {
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.compress());
-    // app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(lessMiddleware(path.join(__dirname, '/public')));
-    app.use(express.static(path.join(__dirname, 'public')));
+  app.set("port", process.env.PORT || 3000);
+  app.set("views", __dirname + "/views");
+  app.set("view engine", "jade");
+  app.use(express.favicon());
+  app.use(express.logger("dev"));
+  app.use(express.compress());
+  // app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(lessMiddleware(path.join(__dirname, "/public")));
+  app.use(express.static(path.join(__dirname, "public")));
 });
 
-app.configure('development', function() {
-    app.use(express.errorHandler());
+app.configure("development", function() {
+  app.use(express.errorHandler());
 });
 
-app.configure('production', function() {
-});
+app.configure("production", function() {});
 
 /*
  * Middleware for parsing raw body data, Express's bodyParser only parses
@@ -41,72 +40,78 @@ app.configure('production', function() {
  * 3. multipart/form-data
  */
 var rawBody = function(req, res, next) {
-    var data = '';
+  var data = "";
 
-    req.setEncoding('utf-8');
+  req.setEncoding("utf-8");
 
-    req.on('data', function(chunk) {
-        data += chunk;
-    });
+  req.on("data", function(chunk) {
+    data += chunk;
+  });
 
-    req.on('end', function() {
-        req.rawBody = data;
-        next();
-    });
+  req.on("end", function() {
+    req.rawBody = data;
+    next();
+  });
 };
 
-app.get('/', function(req, res) {
-    res.render('index', {title: 'Webassets'});
+app.get("/", function(req, res) {
+  res.render("index", { title: "Webassets" });
 });
 
-app.post('/api', rawBody, function(req, res) {
-    var response = 400;
-    var compress = req.query.compress ? true : false;
+app.post("/api", rawBody, function(req, res) {
+  var response = 400;
+  var compress = req.query.compress ? true : false;
 
-    if (req.is('text/css')) {
-        res.type('text/css');
-        response = req.query.compress ? new cleanCSS().minify(req.rawBody) : req.rawBody;
+  if (req.is("text/css")) {
+    res.type("text/css");
+    response = req.query.compress
+      ? new cleanCSS().minify(req.rawBody)
+      : req.rawBody;
+  }
+
+  if (req.is("text/less")) {
+    var parser = new less.Parser();
+
+    parser.parse(req.rawBody, function(err, tree) {
+      if (err) {
+        response = 400;
+        return;
+      }
+      res.type("text/css");
+      response = tree.toCSS({ compress: compress });
+    });
+  }
+
+  if (req.is("text/stylus")) {
+    res.type("text/css");
+    stylus(req.rawBody, { compress: compress }).render(function(err, css) {
+      if (err) {
+        response = 400;
+        return;
+      }
+      response = css;
+    });
+  }
+
+  if (req.is("text/javascript")) {
+    res.type("text/javascript");
+    response = req.query.compress
+      ? uglifyJS.minify(req.rawBody, { fromString: true }).code
+      : req.rawBody;
+  }
+
+  if (req.is("text/coffeescript")) {
+    res.type("text/javascript");
+    response = coffeeScript.compile(req.rawBody);
+
+    if (req.query.compress) {
+      response = uglifyJS.minify(response, { fromString: true }).code;
     }
+  }
 
-    if (req.is('text/less')) {
-        var parser = new(less.Parser)();
-
-        parser.parse(req.rawBody, function(err, tree) {
-            if (err) {
-                return res.send(400);
-            }
-            res.type('text/css');
-            response = tree.toCSS({compress: compress});
-        });
-    }
-
-    if (req.is('text/stylus')) {
-        res.type('text/css');
-        stylus(req.rawBody, {compress: compress}).render(function(err, css) {
-            if (err) {
-                return res.send(400);
-            }
-            response = css;
-        });
-    }
-
-    if (req.is('text/javascript')) {
-        res.type('text/javascript');
-        response = req.query.compress ? uglifyJS.minify(req.rawBody, {fromString: true}).code : req.rawBody;
-    }
-
-    if (req.is('text/coffeescript')) {
-        res.type('text/javascript');
-        response = coffeeScript.compile(req.rawBody);
-
-        if (req.query.compress) {
-            response = uglifyJS.minify(response, {fromString: true}).code;
-        }
-    }
-
-    res.send(response);
+  res.send(response);
 });
 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get("port"), function() {
+  console.log("Express server listening on port " + app.get("port"));
 });
--- test/test.js
+++ test/test.js
@@ -1,151 +1,165 @@
-var request = require('supertest'),
-    uglifyJS = require('uglify-js'),
-    app = require('../app'),
-    uglifyJS = require('uglify-js'),
-    cleanCSS = require('clean-css'),
-    coffeeScript = require('coffee-script');
+var request = require("supertest"),
+  uglifyJS = require("uglify-js"),
+  app = require("../app"),
+  uglifyJS = require("uglify-js"),
+  cleanCSS = require("clean-css"),
+  coffeeScript = require("coffee-script");
 
 request = request(app);
 
-describe('POST /api', function() {
-    describe('Content-Type: text/css', function() {
-        var css = 'a { color: red; }\ndiv { color: black }';
+describe("POST /api", function() {
+  describe("Content-Type: text/css", function() {
+    var css = "a { color: red; }\ndiv { color: black }";
 
-        it('should return the same CSS that was passed in', function(done) {
-            request.post('/api')
-                .type('text/css')
-                .send(css)
-                .expect(200, css)
-                .end(done);
-        });
-
-        it('should return the compressed CSS', function(done) {
-            request.post('/api')
-                .query({compress: true})
-                .type('text/css')
-                .send(css)
-                .expect(200, new cleanCSS().minify(css))
-                .end(done);
-        });
+    it("should return the same CSS that was passed in", function(done) {
+      request
+        .post("/api")
+        .type("text/css")
+        .send(css)
+        .expect(200, css)
+        .end(done);
     });
 
-    describe('Content-Type: text/less', function() {
-        var parser = new(require('less').Parser)();
+    it("should return the compressed CSS", function(done) {
+      request
+        .post("/api")
+        .query({ compress: true })
+        .type("text/css")
+        .send(css)
+        .expect(200, new cleanCSS().minify(css))
+        .end(done);
+    });
+  });
 
-        var less = '@red: #ff0000;\na { color: #00ff00;\n.red { color: @red; }}',
-            cless,  /* Compiled */
-            ccless; /* Compiled and compressed */
+  describe("Content-Type: text/less", function() {
+    var parser = new (require("less")).Parser();
 
-        parser.parse(less, function(e, tree) {
-            cless = tree.toCSS();
-            ccless = tree.toCSS({compress: true});
-        });
+    var less = "@red: #ff0000;\na { color: #00ff00;\n.red { color: @red; }}",
+      cless /* Compiled */,
+      ccless; /* Compiled and compressed */
 
-        it('should return the LESS compiled to CSS', function(done) {
-            request.post('/api')
-                .type('text/less')
-                .send(less)
-                .expect(200, cless)
-                .end(done);
-        });
-
-        it('should return the LESS compiled to CSS and compressed', function(done) {
-            request.post('/api')
-                .query({compress: true})
-                .type('text/less')
-                .send(less)
-                .expect(200, ccless)
-                .end(done);
-        });
-
-        it('should return http 400 on bad input', function(done) {
-            request.post('/api')
-            .type('text/less')
-            .send('some bad css')
-            .expect(400)
-            .end(done);
-        });
+    parser.parse(less, function(e, tree) {
+      cless = tree.toCSS();
+      ccless = tree.toCSS({ compress: true });
     });
 
-    describe('Content-Type: text/stylus', function() {
-        var parser = require('stylus');
-        var stylus = 'form input\n  padding 5px\n  border 1px solid';
-        var compiled, compressed;
-
-        parser(stylus).render(function(err, css) {
-            compiled = css;
-        });
-
-        parser(stylus, {compress: true}).render(function(err, css) {
-            compressed = css;
-        });
-
-
-        it('should return the Stylus compiled to CSS', function(done) {
-            request.post('/api')
-            .type('text/stylus')
-            .send(stylus)
-            .expect(200, compiled)
-            .end(done);
-        });
-
-        it('should return the Stylus compiled to CSS and compressed', function(done) {
-            request.post('/api')
-            .query({compress: true})
-            .type('text/stylus')
-            .send(stylus)
-            .expect(200, compressed)
-            .end(done);
-        });
-
-        it('should return http 400 on bad input', function(done) {
-            request.post('/api')
-            .type('text/stylus')
-            .send('some bad css')
-            .expect(400)
-            .end(done);
-        });
+    it("should return the LESS compiled to CSS", function(done) {
+      request
+        .post("/api")
+        .type("text/less")
+        .send(less)
+        .expect(200, cless)
+        .end(done);
     });
 
-    describe('Content-Type: text/javascript', function() {
-        var js = 'var a = 123;\nfunction test(x) { return x; }';
-
-        it('should return the same JavaScript that was passed in', function(done) {
-            request.post('/api')
-                .type('text/javascript')
-                .send(js)
-                .expect(200, js)
-                .end(done);
-        });
-
-        it('should return the compressed JavaScript', function(done) {
-            request.post('/api')
-                .query({compress: true})
-                .type('text/javascript')
-                .send(js)
-                .expect(200, uglifyJS.minify(js, {fromString: true}).code)
-                .end(done);
-        });
+    it("should return the LESS compiled to CSS and compressed", function(done) {
+      request
+        .post("/api")
+        .query({ compress: true })
+        .type("text/less")
+        .send(less)
+        .expect(200, ccless)
+        .end(done);
     });
 
-    describe('Content-Type: text/coffeescript', function() {
-        var cs = 'alert "I knew it!" if elvis?';
-
-        it('should return the CoffeeScript compiled to regular JavaScript', function(done) {
-            request.post('/api')
-                .type('text/coffeescript')
-                .send(cs)
-                .expect(200, coffeeScript.compile(cs))
-                .end(done);
-        });
-
-        it('should return the CoffeeScript compiled to regular JavaScript and compressed', function(done) {
-            request.post('/api')
-                .query({compress: true})
-                .type('text/coffeescript')
-                .send(cs)
-                .expect(200, uglifyJS.minify(coffeeScript.compile(cs), {fromString: true}).code)
-                .end(done);
-        });
+    it("should return http 400 on bad input", function(done) {
+      request
+        .post("/api")
+        .type("text/less")
+        .send("some bad css")
+        .expect(400)
+        .end(done);
     });
+  });
+
+  describe("Content-Type: text/stylus", function() {
+    var parser = require("stylus");
+    var stylus = "form input\n  padding 5px\n  border 1px solid";
+    var compiled, compressed;
+
+    parser(stylus).render(function(err, css) {
+      compiled = css;
+    });
+
+    parser(stylus, { compress: true }).render(function(err, css) {
+      compressed = css;
+    });
+
+    it("should return the Stylus compiled to CSS", function(done) {
+      request
+        .post("/api")
+        .type("text/stylus")
+        .send(stylus)
+        .expect(200, compiled)
+        .end(done);
+    });
+
+    it("should return the Stylus compiled to CSS and compressed", function(done) {
+      request
+        .post("/api")
+        .query({ compress: true })
+        .type("text/stylus")
+        .send(stylus)
+        .expect(200, compressed)
+        .end(done);
+    });
+
+    it("should return http 400 on bad input", function(done) {
+      request
+        .post("/api")
+        .type("text/stylus")
+        .send("some bad css")
+        .expect(400)
+        .end(done);
+    });
+  });
+
+  describe("Content-Type: text/javascript", function() {
+    var js = "var a = 123;\nfunction test(x) { return x; }";
+
+    it("should return the same JavaScript that was passed in", function(done) {
+      request
+        .post("/api")
+        .type("text/javascript")
+        .send(js)
+        .expect(200, js)
+        .end(done);
+    });
+
+    it("should return the compressed JavaScript", function(done) {
+      request
+        .post("/api")
+        .query({ compress: true })
+        .type("text/javascript")
+        .send(js)
+        .expect(200, uglifyJS.minify(js, { fromString: true }).code)
+        .end(done);
+    });
+  });
+
+  describe("Content-Type: text/coffeescript", function() {
+    var cs = 'alert "I knew it!" if elvis?';
+
+    it("should return the CoffeeScript compiled to regular JavaScript", function(done) {
+      request
+        .post("/api")
+        .type("text/coffeescript")
+        .send(cs)
+        .expect(200, coffeeScript.compile(cs))
+        .end(done);
+    });
+
+    it("should return the CoffeeScript compiled to regular JavaScript and compressed", function(done) {
+      request
+        .post("/api")
+        .query({ compress: true })
+        .type("text/coffeescript")
+        .send(cs)
+        .expect(
+          200,
+          uglifyJS.minify(coffeeScript.compile(cs), { fromString: true }).code
+        )
+        .end(done);
+    });
+  });
 });
